00fa12c9ee538003d0c493834b86c97a03229df6
OlgaKochneva <kochnevaolga74@bk.ru>
2018-11-01 10:23:01 +0300

    Add files via upload

---
+++ Task1.hs
@@ -0,0 +1,89 @@
+--Kochneva Olga 5381
+
+module Text1 where
+
+import Data.Char
+import Data.List
+
+--Task 1
+id_ :: a -> a
+id_ x = x
+
+eval :: (a -> b, a) -> b
+eval (x, y) = x y
+
+exchange :: (a, b) -> (b, a)
+exchange (x, y) = (snd(x, y), fst(x,y))
+
+compose :: (b -> c) -> (a -> b) -> a -> c
+compose x y z = x (y z)
+
+curry_ :: ((a, b) -> c) -> (a -> b -> c)
+curry_ x y z = x (y, z)
+
+associate :: (a, (b, c)) -> ((a, b), c)
+associate (x, pair) = ((x, fst pair), snd pair)
+
+--Task 2
+minMax :: Ord a => [a] -> Maybe (a, a)
+minMax [] = Nothing
+minMax (x : []) = Just (x, x)
+minMax (x : xs)  = Just (helper min (x : xs), helper max (x : xs)) where
+                    helper f [x] = x
+                    helper f (x : xs) = f x (helper f xs)
+
+--Task 3 Stepik 1.6 /// c - count, s - sum
+sum'n'count :: Integer -> (Integer, Integer)
+sum'n'count n | n == 0 = (0, 1)
+              | otherwise = helper (abs n) 0 (0, 0) where
+                   helper 0 i (s, c) = (s + i, c)
+                   helper x i (s, c) = helper (div x 10) (mod x 10) (s + i, c + 1)
+
+--Task 4 Boyer-Moore majority vote algorithm
+--the difficulty islinear, O(n)
+
+majority :: Eq a => [a] -> Maybe a
+majority xs = candidate xs >>= helper xs where
+                               helper :: Eq a => [a] -> a -> Maybe a
+                               helper xs c | major_c c xs = Just c
+                                           | otherwise = Nothing
+
+
+--eliminates all but a single candidate.
+candidate :: Eq a => [a] -> Maybe a
+candidate [] = Nothing
+candidate (x:xs) = Just (find_cand x xs)
+
+--returns the element, that can be majority
+find_cand :: Eq a => a -> [a] -> a
+find_cand cand = fst . foldr step (cand, 1) where
+                                                step x (can, cnt)
+                                                    | can == x  = (can, cnt + 1)
+                                                    | cnt == 0  = (x , 1 )
+                                                    | otherwise = (can, cnt - 1)
+      
+--verify if the element is major
+major_c :: Eq a => a -> [a] -> Bool
+major_c can = (> 0) . foldr step 0 where
+                                        step x cnt
+                                         | can == x  = cnt + 1
+                                         | otherwise = cnt - 1
+
+--Task 5
+f :: (a -> a) -> Int -> (a -> a)
+f g n  | n == 1 = g . id
+       | n > 0  = g . (f g (n - 1))
+       | otherwise = error "n must be positive number"
+
+--Task 6
+fibonacci :: Integer -> Integer
+fibonacci n | n == 0 = 0
+            | n == 1 = 1
+            | n > 0 = mod (fibonacci (n - 1) + fibonacci (n - 2)) 10
+
+--Task 7
+isPalindrome :: String -> Bool
+isPalindrome [] = True
+isPalindrome [x] = True
+isPalindrome (x:xs) | x == last xs = isPalindrome $ init xs
+                    | otherwise = False
